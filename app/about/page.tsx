a80933a482dd28983a4265e40e47781f2c65f3c6
Stefan Djikic <stefandjikic@outlook.com>
2024-02-18 23:55:14 +0100

    Refactor AboutPage component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div className="mt-14">
+      <h1 className="text-3xl font-semibold">About</h1>
+      <h2 className="text-xl my-8">How it started...</h2>
+      <p className="mt-4">
+        My first experience with world of web development was during my
+        highscool. During classes on Information Technologies, besides basic
+        knowledge of computer science, I was introduced to HTML, CSS and Weebly
+        platform. I was fascinated with the idea of creating something out of
+        nothing.
+      </p>
+      <p>
+        At that time, I was playing lead guitar in a rock band, so obviously,
+        for the final project, I decided to create a website for the band using
+        a Weebly platform. 🎸
+      </p>
+      {/* <p>
+        At that time, I was playing lead guitar in a rock band, so my first
+        project was a website for the band using Weebly platform. 🎸
+      </p> */}
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
