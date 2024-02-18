2ee0622085ec894cbebe561dfd93863ae36dc111
Stefan Djikic <stefandjikic@outlook.com>
2024-02-18 12:00:09 +0100

    Refactor Navbar component

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -9,10 +9,11 @@ const Navbar = () => {
   return (
     <nav className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
       <div className="flex justify-between items-center">
-        <Link href="/">
-          <h1 className={`${font.className} text-3xl font-bold text-cyan-100`}>
-            SD
-          </h1>
+        <Link
+          href="/"
+          className={`${font.className} text-3xl font-bold text-cyan-100`}
+        >
+          SD
         </Link>
         <NavMenu />
       </div>
