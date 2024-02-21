9b25fae3fbb4345f829400ea1caf771327fe853d
Stefan Djikic <stefandjikic@outlook.com>
2024-02-21 19:05:57 +0100

    Refactor ProjectsPage component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const ProjectsPage = () => {
   return (
-    <div>ProjectsPage</div>
-  )
-}
+    <div className="mt-14 ">
+      <h1 className="text-3xl text-center font-semibold mb-16 text-cyan-100">
+        Projects
+      </h1>
+    </div>
+  );
+};
 
-export default ProjectsPage
+export default ProjectsPage;
