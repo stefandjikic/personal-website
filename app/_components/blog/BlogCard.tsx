20d791afe123459db58c3c1ca880ebd52f826c31
Stefan Djikic <stefandjikic@outlook.com>
2024-03-02 11:02:28 +0100

    Add formatted date to BlogCard component

--- app/_components/blog/BlogCard.tsx
+++ app/_components/blog/BlogCard.tsx
@@ -1,3 +1,4 @@
+import { getFormattedDate } from "@/app/utils/helpers";
 import { IBlogPostMetaData } from "@/app/utils/interfaces";
 import Link from "next/link";
 import React from "react";
@@ -7,12 +8,14 @@ interface BlogCardProps {
 }
 
 const BlogCard = ({ post }: BlogCardProps) => {
+  const formattedDate = getFormattedDate(post?.date);
+
   return (
     <div className="rounded-md backdrop-blur-sm p-4 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l">
       <Link href={`/blog/${post.slug}`}>
         <p className="text-xs mb-2">{post?.category}</p>
         <h4 className="text-lg font-bold text-cyan-100">{post.title}</h4>
-        <p className="text-sm mt-2">{post.date}</p>
+        <p className="text-sm mt-2">{formattedDate}</p>
       </Link>
     </div>
   );
