5aca9da7e5df9ad287502e662e1f1dc03b628c9d
Stefan Djikic <stefandjikic@outlook.com>
2024-12-07 00:19:07 +0100

    Update PROJECTS_DATA to include new personal project and adjust IDs

--- app/utils/projects_data.ts
+++ app/utils/projects_data.ts
@@ -3,11 +3,20 @@ import medidredImg from "@/app/assets/images/medidred.webp";
 import ceImg from "@/app/assets/images/ce-screen.webp";
 
 export const PROJECTS_DATA = {
-  title: "Projects",
+  title: "Personal Projects",
   description: "Here are some of the personal projects I've worked on.",
   projects: [
     {
       id: 1,
+      name: 'Next.js Blog & CMS Template',
+      description:
+        'A template for creating a blog with a CMS using Next.js and Sanity. The template includes a blog with posts, tags and comments. The CMS is used for managing posts, comments and tags.',
+      projectUrl: 'https://next-cms-blog-ce.vercel.app/',
+      sourceUrl: 'https://github.com/stefandjikic/next-cms-blog',
+      projectImage: 'https://github.com/stefandjikic/next-cms-blog/assets/65912958/2576b109-d592-4321-9870-0db29d769a3e'
+    },
+    {
+      id: 2,
       name: "Kulijana",
       description:
         "A news-like platform where users can read about interseting topics from around the world. The app is built wiht Next.js, GraphQl, Chakra UI and it is powerd by Hygraph CMS. All data, includiing articles, categories and navigation items are managed from the CMS.",
@@ -16,7 +25,7 @@ export const PROJECTS_DATA = {
       projectImage: kulijanaImg,
     },
     {
-      id: 2,
+      id: 3,
       name: "Med i Dred",
       description:
         "A website with e-commerce solution, created for a YouTube channel that focuses on beekeeping and honey production. The website is built with Next.js, Tailwind and it is powerd by Sanity CMS. The website also utilizes Sanity for managing orders and products on the website.",
@@ -25,7 +34,7 @@ export const PROJECTS_DATA = {
       projectImage: medidredImg,
     },
     {
-      id: 3,
+      id: 4,
       name: "YouTube Channel - Codewalk Empire",
       description:
         "A YouTube channel where I share my knowledge and experience with web development. I create tutorials, code walkthroughs and other content related to web development and programming. Visit the channel to check out the content I've created so far and subscribe if you like it :)",
