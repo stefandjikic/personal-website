f8aa14f9a4747d2b60c761ce075e1131d9c14758
Stefan Djikic <stefandjikic@outlook.com>
2024-12-07 12:57:05 +0100

    Replace Blog section title with a link to the blog page and remove unused code

--- app/_components/home/BlogSection.tsx
+++ app/_components/home/BlogSection.tsx
@@ -1,25 +1,20 @@
 import React from "react";
-import { POSTS } from "@/app/helpers";
 import BlogCard from "../blog/BlogCard";
 import { getLatestPosts } from "@/app/blog/_getData";
+import Link from "next/link";
 
 const BlogSection = () => {
   const latestPosts = getLatestPosts(3);
   return (
     <div className="my-10">
       <h3 className="text-xl uppercase text-cyan-100 text-center mb-10">
-        Blog
+        <Link href="/blog">Blog</Link>
       </h3>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {latestPosts.map((post) => (
           <BlogCard key={post.slug} post={post} />
         ))}
-      </div>
-      <div>
-        {/* TODO: Create Button Component */}
-        {/* <button className="text-cyan-100 font-semibold mt-8">
-          See more
-        </button> */}
       </div>
     </div>
   );
