4963db490c7c56d767b4cef869e7790c39ec4f95
Stefan Djikic <stefandjikic@outlook.com>
2024-12-21 14:28:54 +0100

    Refactor HeroComponent: comment out unused font import and clean up spacing in JSX

--- app/_components/home/HeroComponent.tsx
+++ app/_components/home/HeroComponent.tsx
@@ -3,14 +3,12 @@ import GradientComponent from "../gradients/GradientComponent";
 import Image from "next/image";
 import hiIcon from "@/app/assets/icons/hi-y.svg";
 import me from "@/app/assets/images/me-4.jpg";
-import { Lilita_One } from "next/font/google";
 import SocialButtons from "../social/SocialButtons";
 
-const font = Lilita_One({ weight: "400", subsets: ["latin"] });
+// const font = Lilita_One({ weight: "400", subsets: ["latin"] });
 
 const HeroComponent = () => {
   return (
-    // min-h-[54vh] mt-10
     <div className="flex flex-col md:flex-row gap-6 justify-between items-center min-h-[44vh] mt-20 relative">
       <div>
         <GradientComponent />
@@ -21,11 +19,15 @@ const HeroComponent = () => {
         <h2 className="text-4xl sm:text-5xl font-semibold mb-2">
           I&apos;m <span className={` text-cyan-100 font-bold`}>Stefan</span>
         </h2>
-        <p className="mt-6 text-2xl sm:text-3xl ">I&apos;m a software developer.</p>
+        <p className="mt-6 text-2xl sm:text-3xl ">
+          I&apos;m a software developer.
+        </p>
         <p className="mb-2 text-xl sm:text-2xl">
           I love building stuff and solving problems.
         </p>
-        <p className="mt-6 text-lg sm:text-2xl my-4 mb-10">Welcome to my space!</p>
+        <p className="mt-6 text-lg sm:text-2xl my-4 mb-10">
+          Welcome to my space!
+        </p>
         <SocialButtons />
       </div>
       <div className="backdrop-blur-sm bg-white/5 rounded-md relative ">
