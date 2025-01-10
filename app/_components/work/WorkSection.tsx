0da2aad3864e240f06fbe5ec6f3c664a24e09653
Stefan Djikic <stefandjikic@outlook.com>
2025-01-10 20:41:57 +0100

    Add WorkSection component to display work experience details

---
+++ app/_components/work/WorkSection.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+interface WorkSectionProps {
+  company: string;
+  role: string;
+  type: string;
+  image: any;
+  imageAlt: string;
+  link: string;
+  imgSize?: number;
+  imgRounded?: boolean;
+  mobileImgWidth?: number;
+  mobileImgHeight?: number;
+  children?: React.ReactNode;
+}
+
+const WorkSection: React.FC<WorkSectionProps> = ({
+  company,
+  role,
+  type,
+  image,
+  imageAlt,
+  link,
+  imgSize = 60,
+  imgRounded = false,
+  mobileImgWidth = 40,
+  mobileImgHeight = 40,
+  children,
+}) => {
+  return (
+    <section className="mb-20">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 justify-between items-center mb-10">
+        <div className="col-span-2">
+          <div className="mb-6 flex gap-4 items-center">
+            <div className="flex sm:hidden justify-center items-center w-[60px] h-[60px] rounded-md backdrop-blur-sm p-2 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
+              <a href={link} target="_blank" rel="noreferrer">
+                <Image
+                  src={image}
+                  alt={imageAlt}
+                  className={`${imgRounded ? 'rounded-full' : ''} group-hover:scale-105 transition duration-200`}
+                  width={imgSize}
+                  height={imgSize}
+                />
+              </a>
+            </div>
+            <div>
+              <h2 className="text-cyan-50/70 text-sm">
+                {role} · <span className="text-xs">{type}</span>
+              </h2>
+              <h3 className="text-2xl font-bold text-cyan-100">{company}</h3>
+            </div>
+          </div>
+          {children}
+        </div>
+        <div className="hidden sm:flex justify-center items-center w-[200px] h-[200px] ml-auto rounded-md backdrop-blur-sm p-4 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
+          <a href={link} target="_blank" rel="noreferrer">
+            <Image
+              src={image}
+              alt={imageAlt}
+              className={`${imgRounded ? 'rounded-full' : ''} group-hover:scale-105 transition duration-200`}
+              width={mobileImgWidth}
+              height={mobileImgHeight}
+            />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkSection;
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import { Metadata } from "next";
 import k7techImg from "@/app/assets/images/k7_logo.png";
 import codeImg from "@/app/assets/images/code_logo.png";
 import rawfishImg from "@/app/assets/images/rawfish_logo.png";
+import WorkSection from "../_components/work/WorkSection";
 
 export const metadata: Metadata = {
   title: "Work",
@@ -15,145 +15,68 @@ const WorkPage = () => {
       <h1 className="text-3xl text-center font-semibold mb-16 text-cyan-100">
         Work Experience
       </h1>
-      <section className="mb-20">
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 justify-between items-center mb-10">
-          <div className="col-span-2">
-            <div className="mb-6 flex gap-4 items-center">
-              <div className="flex sm:hidden justify-center items-center w-[60px] h-[60px] rounded-md backdrop-blur-sm p-2 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
-                <a href="https://rawfish.com/en/" target="_blank" rel="noreferrer">
-                  <Image
-                    src={rawfishImg}
-                    alt="Rawfish"
-                    className="group-hover:scale-105 transition duration-200"
-                    width={40}
-                    height={40}
-                  />
-                </a>
-              </div>
-              <div>
-                <h2 className="text-cyan-50/70 text-sm">
-                  React Native Developer ·{" "}
-                  <span className="text-xs">Contract</span>
-                </h2>
-                <h3 className="text-2xl font-bold text-cyan-100">Rawfish</h3>
-              </div>
-            </div>
-            <p className="leading-7 font-light">
-              Participate in developing the Amazon Fire TV app for one of the
-              largest Italian mass media companies.
-            </p>
-          </div>
-          <div className="hidden sm:flex  justify-center items-center w-[200px] h-[200px] ml-auto rounded-md backdrop-blur-sm p-4 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
-            <a href="https://rawfish.com/en/" target="_blank" rel="noreferrer">
-              <Image
-                src={rawfishImg}
-                alt="Rawfish"
-                className="group-hover:scale-105 transition duration-200"
-                width={150}
-                height={50}
-              />
-            </a>
-          </div>
-        </div>
-      </section>
-
-      <section className="mb-20">
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 justify-between items-center mb-10">
-          <div className="col-span-2">
-            <div className="mb-6 flex gap-4 items-center">
-              <div className="flex sm:hidden justify-center items-center w-[60px] h-[60px] rounded-md backdrop-blur-sm p-2 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
-                <a
-                  href="https://k7tech.agency/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Image
-                    src={k7techImg}
-                    alt="K7 Tech"
-                    className="rounded-full group-hover:scale-105 transition duration-200"
-                    width={60}
-                    height={60}
-                  />
-                </a>
-              </div>
-              <div>
-                <h2 className="text-cyan-50/70 text-sm">
-                  Frontend Developer ·{" "}
-                  <span className="text-xs">Full Time</span>
-                </h2>
-                <h3 className="text-2xl font-bold text-cyan-100">K7 Tech</h3>
-              </div>
-            </div>
-
-            <p className="leading-7 font-light">
-              At K7Tech, I work as a full-time frontend developer, actively
-              participating in all stages of web and mobile app development. I
-              collaborate closely with clients during the project planning phase
-              to understand their needs and expectations. Using my expertise in
-              React, Next.js, and React Native, I build high-quality web and
-              mobile apps.
-            </p>
-          </div>
-          <div className="hidden sm:flex justify-center items-center w-[200px] h-[200px] ml-auto rounded-md backdrop-blur-sm p-4 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
-            <a href="https://k7tech.agency/" target="_blank" rel="noreferrer">
-              <Image
-                src={k7techImg}
-                alt="K7 Tech"
-                className="rounded-md group-hover:scale-105 transition duration-200"
-                width={200}
-                height={200}
-              />
-            </a>
-          </div>
-        </div>
-      </section>
-
-      <section className="mb-20">
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 justify-between items-center mb-10">
-          <div className="col-span-2">
-            <div className="mb-6 flex gap-4 items-center">
-              <div className="flex sm:hidden justify-center items-center w-[60px] h-[60px] rounded-md backdrop-blur-sm p-2 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
-                <a href="https://code.edu.rs/" target="_blank" rel="noreferrer">
-                  <Image
-                    src={codeImg}
-                    alt="Code by Comtrade"
-                    className="rounded-full group-hover:scale-105 transition duration-200"
-                    width={40}
-                    height={40}
-                  />
-                </a>
-              </div>
-              <div>
-                <h2 className="text-cyan-50/70 text-sm">
-                  Frontend Development Course Lecturer ·{" "}
-                  <span className="text-xs">Contract</span>
-                </h2>
-                <h3 className="text-2xl font-bold text-cyan-100">
-                  Code by Comtrade
-                </h3>
-              </div>
-            </div>
-            <p className="leading-7 font-light">
-              At Code By Comtrade, I had the privilege of sharing my knowledge
-              and expertise with aspiring developers. As a course lecturer, I
-              organized and taught courses on JavaScript, React, HTML, and CSS.
-              I actively participated in student grading and had the
-              responsibility of evaluating their final exams and projects.
-            </p>
-          </div>
-          <div className="hidden sm:flex  justify-center items-center w-[200px] h-[200px] ml-auto rounded-md backdrop-blur-sm p-4 bg-gradient-to-r from-indigo-600/20 to-cyan-800/20 hover:bg-gradient-to-l border border-cyan-300/10 hover:border-cyan-300/20 group transition duration-200">
-            <a href="https://code.edu.rs/" target="_blank" rel="noreferrer">
-              <Image
-                src={codeImg}
-                alt="Code by Comtrade"
-                className="rounded-full group-hover:scale-105 transition duration-200"
-                width={150}
-                height={50}
-              />
-            </a>
-          </div>
-        </div>
-      </section>
+      <WorkSection
+        company="Rawfish"
+        role="React Native Developer"
+        type="Contract"
+        image={rawfishImg}
+        imageAlt="Rawfish"
+        link="https://rawfish.com/en/"
+        imgSize={40}
+        mobileImgWidth={150}
+        mobileImgHeight={50}
+      >
+        <p className="leading-7 font-light">
+          Participating in development of the Amazon Fire TV app for{" "}
+          <a
+            href="https://mediasetinfinity.mediaset.it/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold"
+          >
+            Mediaset.
+          </a>
+        </p>
+      </WorkSection>
+      <WorkSection
+        company="K7 Tech"
+        role="Frontend Developer"
+        type="Full Time"
+        image={k7techImg}
+        imageAlt="K7 Tech"
+        link="https://k7tech.agency/"
+        imgSize={60}
+        mobileImgWidth={200}
+        mobileImgHeight={200}
+      >
+        <p className="leading-7 font-light">
+          At K7Tech, I work as a full-time frontend developer, actively
+          participating in all stages of web and mobile app development. I
+          collaborate closely with clients during the project planning phase to
+          understand their needs and expectations. Using my expertise in React,
+          Next.js, and React Native, I build high-quality web and mobile apps.
+        </p>
+      </WorkSection>
+      <WorkSection
+        company="Code by Comtrade"
+        role="Frontend Development Course Lecturer"
+        type="Contract"
+        image={codeImg}
+        imageAlt="Code by Comtrade"
+        link="https://code.edu.rs/"
+        imgSize={40}
+        imgRounded
+        mobileImgWidth={150}
+        mobileImgHeight={50}
+      >
+        <p className="leading-7 font-light">
+          At Code By Comtrade, I had the privilege of sharing my knowledge and
+          expertise with aspiring developers. As a course lecturer, I organized
+          and taught courses on JavaScript, React, HTML, and CSS. I actively
+          participated in student grading and had the responsibility of
+          evaluating their final exams and projects.
+        </p>
+      </WorkSection>
     </div>
   );
 };
