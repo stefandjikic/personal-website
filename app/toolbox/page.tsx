521f51867c25e277c1fad08eb24e3c063037a19a
Stefan Djikic <stefandjikic@outlook.com>
2025-01-11 22:47:04 +0100

    Add ToolboxPage component to showcase tools and resources

---
+++ app/toolbox/page.tsx
@@ -0,0 +1,22 @@
+const ToolboxPage = () => {
+  return (
+    <div className="mt-14 ">
+      <h1 className="text-3xl text-center font-semibold mb-16 text-cyan-100">
+        What I Use
+        <span
+          className="
+          text-sm
+          font-normal
+          block
+          mt-4
+        "
+        >
+          {" "}
+          👷‍♂️🛠️ in progress...{" "}
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default ToolboxPage;
